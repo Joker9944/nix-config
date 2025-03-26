af9738e35d27e42fb9a44a17f9c93658a0419b2b
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-03-26 11:32:37 +0100

    Add missing dconf entries

--- users/joker9944/config/gnome.nix
+++ users/joker9944/config/gnome.nix
@@ -1,11 +1,15 @@
 { lib, config, pkgs, ...}:
 
-with lib; let
+with lib; with lib.hm.gvariant; let
   mkAutoMoveWindowsApplicationList = attrs: attrValues ( mapAttrs ( app: index: app + ":" + toString index ) attrs );
 in {
-  dconf.settings = with lib.gvariant; {
+  dconf.settings = {
+
     # Theming
-    "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      accent-color = "purple";
+    };
 
     # Keybindings
     "org/gnome/desktop/wm/keybindings" = {
@@ -18,6 +22,10 @@ in {
     };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       screensaver = [ "<Super>Escape" ];
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+      ];
     };
     "org/gnome/settings-daemon/plugins/custom-keybindings/custom0" = {
         name = "Console";
@@ -33,12 +41,35 @@ in {
       restore-shortcuts = mkEmptyArray type.string;
     };
 
+    # Weather
+    "org/gnome/GWeather4" = {
+      temperature-unit = "centigrade";
+    };
+    "org/gnome/shell/weather" = {
+      automatic-location = true;
+    };
+
     # Extensions
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enabled-extensions = [
+        "clipboard-history@alexsaveau.dev"
+        "auto-move-windows@gnome-shell-extensions.gcampax.github.com"
+        "dash-to-panel@jderose9.github.com"
+        "tophat@fflewddur.github.io"
+        "dash-to-dock@micxgx.gmail.com"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+      ];
+    };
     "org/gnome/shell/extensions/dash-to-dock" = {
+      dock-position = "LEFT"; # Position on screen
+      dock-fixed = true; # ! Intelligent autohide
+      extend-height = true; # Panel mode
       always-center-icons = true;
-      apply-custom-theme = true;
-      dock-position = "LEFT";
+      dash-max-icon-size = 32; # Icon size limit
       show-trash = false;
+      apply-custom-theme = true; # Use built-in theme
     };
     "org/gnome/shell/extensions/auto-move-windows" = {
       application-list = mkAutoMoveWindowsApplicationList {
@@ -48,6 +79,11 @@ in {
         "code.desktop" = 3;
         "spotify.desktop" = 4;
       };
+    };
+    "org/gnome/shell/extensions/tophat" = {
+      fs-display = "numeric";
+      mem-display = "numeric";
+      network-usage-unit = "bits";
     };
   };
 }
