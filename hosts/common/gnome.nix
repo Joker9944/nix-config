f31e14467fc4c2d8165e4f81d625a0dd42eaa19e
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-05-12 11:36:47 +0200

    Optimize gnome config

--- hosts/common/gnome.nix
+++ hosts/common/gnome.nix
@@ -23,22 +23,5 @@ in {
       gnome-connections
       epiphany # web browser
     ];
-
-    environment.systemPackages = with pkgs; [
-      gnomeExtensions.tophat
-      libgtop # Dependency for TopHat
-      clutter # Dependency for TopHat
-      gnomeExtensions.clipboard-history
-      gnomeExtensions.auto-move-windows
-      gnomeExtensions.dash-to-dock
-      dconf-editor
-      gparted
-    ];
-
-    environment.variables = {
-      # TODO Workaround for missing TopHat dependency
-      # https://github.com/fflewddur/tophat/issues/106#issuecomment-1848319826
-      GI_TYPELIB_PATH = "/run/current-system/sw/lib/girepository-1.0";
-    };
   };
 }
