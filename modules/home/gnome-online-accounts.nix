d54b54fea19668e17b15053df17e3e4d8c9d8062
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-05-19 11:16:49 +0200

    Add gnome online acccounts home module
    commit 159a77b7adb2c559607e2196416cca1b9792f05f
    Author: Joker9944 <9194199+Joker9944@users.noreply.github.com>
    Date:   Mon May 19 11:15:38 2025 +0200
    
        Add gnome online accounts home module

---
+++ modules/home/gnome-online-accounts.nix
@@ -0,0 +1,39 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.programs.gnome-online-accounts;
+in {
+  options.programs.gnome-online-accounts = with lib; {
+    enable = mkEnableOption "Whether to enable GNOME online accounts config.";
+    accounts = mkOption {
+      type = with types; attrsOf attrs;
+      default = {};
+      example = literalExpression ''
+        {
+          "account_1745833011_0" = {
+            Provider = "google";
+            Identity = "example@gmail.com";
+            PresentationIdentity = "example@gmail.com";
+            MailEnabled = true;
+            CalendarEnabled = true;
+            ContactsEnabled = true;
+            FilesEnabled = true;
+          };
+        };
+      '';
+      description = ''
+        Online accounts to be added.
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    xdg.configFile."goa-1.0/accounts.conf".text =
+      lib.generators.toINI {
+        mkSectionName = name: lib.escape ["[" "]"] ("Account " + name);
+      }
+      cfg.accounts;
+  };
+}
