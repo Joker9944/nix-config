37ab802053a43f655e6af506507a8fe193e966ed
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-07-31 17:41:54 +0200

    Optimize home manager usage

--- users/joker9944/hosts/HAL9000.nix
+++ users/joker9944/hosts/HAL9000.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  osConfig,
+  ...
+}: {
   # WORKAROUND home-manager does not supply a way to set autostart apps
   # https://github.com/nix-community/home-manager/issues/3447
   # TODO upgrade once stable
@@ -13,12 +18,19 @@
 
   home.packages = with pkgs; [
     prismlauncher
-    lutris
   ];
 
-  programs.btop.package = pkgs.btop-cuda;
+  programs = {
+    btop.package = pkgs.btop-cuda;
 
-  programs._1password.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9R2V8FqyXifBoVO3OndfpRrqxdwK1H/3qlm645l7rg";
+    _1password.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP9R2V8FqyXifBoVO3OndfpRrqxdwK1H/3qlm645l7rg";
+
+    lutris = {
+      enable = true;
+      extraPackages = with pkgs; [mangohud winetricks gamemode umu-launcher];
+      protonPackages = [pkgs-unstable.proton-ge-bin];
+    };
+  };
 
   gnome-settings.peripherals.touchpad.enable = false;
 }
