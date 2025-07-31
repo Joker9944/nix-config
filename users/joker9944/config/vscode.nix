4f6b0a448a8ba2fd8981ace68a6e9a6ff91bf23d
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-07-31 19:01:27 +0200

    Refactor vscode config module

--- users/joker9944/config/vscode.nix
+++ users/joker9944/config/vscode.nix
@@ -3,74 +3,140 @@
   pkgs,
   ...
 }: let
-  commonExtensions = with pkgs.vscode-extensions; [
-    dracula-theme.theme-dracula
-    streetsidesoftware.code-spell-checker
-    streetsidesoftware.code-spell-checker-swiss-german
-    esbenp.prettier-vscode
+  commonProfiles = [
+    {
+      # vscode settings
+      userSettings = {
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.fontFamily" = "'JetBrains Mono', 'Adwaita Mono', monospace";
+        "editor.renderWhitespace" = "boundary";
+        "editor.tabSize" = 2;
+        "files.autoSave" = "afterDelay";
+        "files.insertFinalNewline" = true;
+        "git.confirmSync" = false;
+        "git.blame.editorDecoration.enabled" = true;
+        "git.autofetch" = true;
+      };
+    }
+    {
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+      ];
+
+      userSettings."workbench.colorTheme" = "Dracula Theme";
+    }
+    {
+      extensions = with pkgs.vscode-extensions; [
+        streetsidesoftware.code-spell-checker
+        streetsidesoftware.code-spell-checker-swiss-german
+      ];
+
+      userSettings."cSpell.language" = "en,de-CH";
+    }
+    {
+      extensions = with pkgs.vscode-extensions; [
+        esbenp.prettier-vscode
+      ];
+
+      userSettings = {
+        "[markdown]" = {
+          "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        };
+      };
+    }
+    {
+      extensions = with pkgs.vscode-extensions; [
+        k--kato.intellij-idea-keybindings
+      ];
+    }
   ];
 
-  commonSettings = {
-    "files.autoSave" = "afterDelay";
-    "files.insertFinalNewline" = true;
-    "editor.tabSize" = 2;
-    "editor.renderWhitespace" = "boundary";
-    "diffEditor.ignoreTrimWhitespace" = false;
-    "workbench.colorTheme" = "Dracula Theme";
-    "editor.fontFamily" = "'JetBrains Mono', 'Adwaita Mono', monospace";
-    "[markdown]" = {
-      "editor.defaultFormatter" = "esbenp.prettier-vscode";
-    };
-    "cSpell.language" = "en,de-CH";
-    "git.confirmSync" = false;
-  };
+  mergeProfiles = profiles:
+    lib.lists.foldr (el: acc:
+      (lib.recursiveUpdate acc el)
+      // {
+        extensions = acc.extensions ++ (el.extensions or []);
+      }) {
+      extensions = [];
+      userSettings = {};
+    }
+    profiles;
 
-  mkProfile = profile:
-    profile
-    // {
-      extensions = commonExtensions ++ profile.extensions or [];
-      userSettings = lib.recursiveUpdate commonSettings profile.userSettings or {};
-    };
+  mkProfile = profiles:
+    if lib.lists.isList profiles
+    then mergeProfiles (commonProfiles ++ profiles)
+    else mergeProfiles (commonProfiles ++ [profiles]);
 in {
-  config.programs.vscode = {
-    package = pkgs.vscodium;
+  config = {
+    home.packages = with pkgs; [
+      jetbrains-mono # default -> font configured
+      sops # default -> used for git secret encryption
+      alejandra # nix -> alejandra extension
+      kubectl # k8s -> kubernetes tooling
+    ];
 
-    profiles = {
-      default = mkProfile {
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
-      };
+    programs.vscode = {
+      package = pkgs.vscodium;
 
-      nix = mkProfile {
-        extensions = with pkgs.vscode-extensions; [
-          kamadorueda.alejandra
-          jnoortheen.nix-ide
-        ];
-
-        userSettings = {
-          "[nix]" = {
-            "editor.tabSize" = 2;
-          };
+      profiles = {
+        default = mkProfile {
+          enableUpdateCheck = false;
+          enableExtensionUpdateCheck = false;
         };
-      };
 
-      notes = mkProfile {
-        extensions = with pkgs.vscode-extensions; [
-          foam.foam-vscode
-          yzhang.markdown-all-in-one
+        nix = mkProfile [
+          {
+            extensions = with pkgs.vscode-extensions; [
+              jnoortheen.nix-ide
+            ];
+
+            userSettings = {
+              "[nix]" = {
+                "editor.tabSize" = 2;
+              };
+            };
+          }
+          {
+            extensions = with pkgs.vscode-extensions; [
+              kamadorueda.alejandra
+            ];
+
+            userSettings = {
+              "[nix]" = {
+                "editor.defaultFormatter" = "kamadorueda.alejandra";
+                "editor.formatOnPaste" = true;
+                "editor.formatOnSave" = true;
+                "editor.formatOnType" = false;
+              };
+              "alejandra.program" = "alejandra";
+            };
+          }
         ];
-      };
 
-      k8s = mkProfile {
-        extensions = with pkgs.vscode-extensions; [
-          ms-kubernetes-tools.vscode-kubernetes-tools
-          ms-vscode-remote.remote-containers
-          redhat.vscode-yaml
-        ];
-
-        userSettings = {
-          "redhat.telemetry.enabled" = false;
+        notes = mkProfile {
+          extensions = with pkgs.vscode-extensions; [
+            foam.foam-vscode
+            yzhang.markdown-all-in-one
+          ];
         };
+
+        k8s = mkProfile [
+          {
+            extensions = with pkgs.vscode-extensions; [
+              ms-kubernetes-tools.vscode-kubernetes-tools
+              ms-vscode-remote.remote-containers
+            ];
+          }
+          {
+            extensions = with pkgs.vscode-extensions; [
+              redhat.vscode-yaml
+            ];
+
+            userSettings = {
+              "redhat.telemetry.enabled" = false;
+            };
+          }
+        ];
       };
     };
   };
