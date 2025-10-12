c14bfbe14b75b770874f6442515ed38fc61896cc
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-10-12 17:02:56 +0200

    WIP helper

---
+++ modules/home/command-collection/package.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  python3Packages,
+  writeScript,
+  code,
+  ...
+}:
+python3Packages.buildPythonApplication {
+  pname = "command-collection-helper";
+  version = "1.0.0";
+  pyproject = false; # cSpell:ignore pyproject dontUnpack typer
+  dontUnpack = true;
+
+  src = writeScript "command-collection-helper.py" lib.concatLines [
+    "#!/usr/bin/env python3"
+    code
+  ];
+
+  dependencies = with python3Packages; [ typer ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/command-collection-helper
+  '';
+
+  meta.mainProgram = "command-collection-helper";
+}
