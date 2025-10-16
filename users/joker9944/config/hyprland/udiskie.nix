0e021dbc49b0038d73852af251262ee89bea79e3
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-10-16 15:33:53 +0200

    udiskie: add commands

--- users/joker9944/config/hyprland/udiskie.nix
+++ users/joker9944/config/hyprland/udiskie.nix
@@ -2,16 +2,21 @@
   * mount daemon
 */
 {
+  pkgs-hyprland,
   config,
   utility,
   ...
 }:
 utility.custom.mkHyprlandModule config {
+  home.packages = [ pkgs-hyprland.udiskie ];
+
   services = {
     udiskie = {
       enable = true;
       # TODO Not supported in release-25.05 simplify when updating to release-25.11
       #package = pkgs-hyprland.udiskie;
+
+      tray = false;
     };
   };
 }
