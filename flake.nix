36b935fcda6c157d4c5339564cc499caee43c86a
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2025-11-04 14:56:35 +0100

    substituters: move substituters to flake config

--- flake.nix
+++ flake.nix
@@ -224,6 +224,23 @@
         }
       );
 
+      nixConfig = {
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://hyprland.cachix.org"
+        ];
+
+        trusted-substituters = [
+          "https://nix-community.cachix.org"
+          "https://hyprland.cachix.org"
+        ];
+
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # cSpell:disable-line
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" # cSpell:disable-line
+        ];
+      };
+
       nixosConfigurations = lib.attrsets.listToAttrs (
         lib.lists.map
           (cfg: {
--- hosts/default.nix
+++ hosts/default.nix
@@ -32,20 +32,6 @@ in
         "nix-command"
         "flakes"
       ];
-
-      # Third-party Cachix
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-      ];
-      trusted-substituters = [
-        "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # cSpell:disable-line
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" # cSpell:disable-line
-      ];
     };
 
     # Enable automatic nix store garbage collection
