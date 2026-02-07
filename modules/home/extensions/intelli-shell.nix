0e8672fadf87e1cf76e17ea9a190d71ad06d4ff5
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-02-07 18:25:37 +0100

    intelli-shell: add declarative commands

--- modules/home/extensions/intelli-shell.nix
+++ modules/home/extensions/intelli-shell.nix
@@ -7,16 +7,43 @@
 {
   options.programs.intelli-shell =
     let
-      inherit (lib) mkOption types;
+      inherit (lib) mkOption types literalExpression;
     in
     {
+      defaultTag = mkOption {
+        type = types.str;
+        default = "managed";
+        description = ''
+          Tag to be added to all commands to indicate that they are managed through this module.
+        '';
+      };
+
       commands = mkOption {
         type = types.listOf (
           types.submodule {
             options = {
-              alias = mkOption { type = types.str; };
-              command = mkOption { type = types.str; };
-              description = mkOption { type = types.str; };
+              command = mkOption {
+                type = types.str;
+                example = "sudo nixos-rebuild switch --flake {{~/Workspace/nix-config|github:joker9944/nix-config}}";
+              };
+
+              alias = mkOption {
+                type = types.nullOr types.str;
+                default = null;
+                example = "nix update";
+              };
+
+              description = mkOption {
+                type = types.nullOr types.str;
+                default = null;
+                example = "Update nix with flake";
+              };
+
+              tags = mkOption {
+                type = types.listOf types.str;
+                default = [ ];
+                example = literalExpression "[ \"nix\" ]";
+              };
             };
           }
         );
@@ -27,23 +54,38 @@
   config.home.activation =
     let
       cfg = config.programs.intelli-shell;
+
+      mkCommand =
+        command:
+        let
+          tags = [ cfg.defaultTag ] ++ command.tags;
+          mkCommandHeader =
+            lib.pipe
+              [
+                (lib.optional (command.alias != null) "[alias:${command.alias}]")
+                (lib.optional (command.description != null) command.description)
+                (lib.map (tag: "#${tag}") tags)
+              ]
+              [
+                lib.flatten
+                (lib.concatStringsSep " ")
+                (parts: "# ${parts}\n")
+              ];
+        in
+        mkCommandHeader + command.command;
     in
     lib.mkIf (cfg.enable && cfg.commands != [ ]) {
       # cSpell:words Intelli
       importIntelliShellCommands =
         let
-          bin.intelli-shell = lib.getExe config.programs.intelli-shell.package;
-          commands = lib.pipe cfg.commands [
-            (lib.map (command: ''
-              # [alias:${command.alias}] ${command.description}
-              ${command.command}
-            ''))
+          imports = lib.pipe cfg.commands [
+            (lib.map mkCommand)
             lib.concatLines
-            (pkgs.writeText "commands.sh")
+            (pkgs.writeText "imports")
           ];
         in
         lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          ${bin.intelli-shell} import --file ${commands}
+          ${lib.getExe config.programs.intelli-shell.package} import --file ${imports}
         '';
     };
 }
--- users/mixins/default.nix
+++ users/mixins/default.nix
@@ -46,5 +46,63 @@
         url."git@github.com:".insteadOf = "https://github.com/";
       };
     };
+
+    # TODO move to better location
+    intelli-shell.commands = [
+      {
+        command = "git commit --amend --no-edit";
+        alias = "git commit amend";
+        description = "Amend the current commit without editing the message";
+        tags = [ "git" ];
+      }
+      {
+        command = "sudo nixos-rebuild switch --flake {{~/Workspace/nix-config|github:joker9944/nix-config}}";
+        alias = "nix update";
+        description = "Update nix with flake";
+        tags = [ "nixos" ];
+      }
+      {
+        command = "home-manager switch --flake {{~/Workspace/nix-config|github:joker9944/nix-config}}";
+        alias = "home update";
+        description = "Update home with flake";
+        tags = [ "hm" ];
+      }
+      {
+        command = "home-manager news --flake {{~/Workspace/nix-config|github:joker9944/nix-config}}";
+        alias = "home news";
+        description = "Show home news";
+        tags = [ "hm" ];
+      }
+      {
+        command = "nix repl {{nixpkgs|github:joker9944/nix-config}}";
+        alias = "nix shell";
+        description = "Start interactive nix shell";
+        tags = [ "nix" ];
+      }
+      {
+        command = "nix search {{nixpkgs|github:joker9944/nix-config}} {{regex}}";
+        alias = "nix search package";
+        description = "Search installable for a nix package";
+        tags = [ "nix" ];
+      }
+      {
+        command = "nixos-option --flake {{~/Workspace/nix-config|github:joker9944/nix-config}} {{option}}";
+        alias = "nix search option";
+        description = "Search set option values from flake";
+        tags = [ "nix" ];
+      }
+      {
+        command = "sops --indent {{2}} --decrypt --in-place {{file}}";
+        alias = "sops decrypt";
+        description = "Decrypt file with sops";
+        tags = [ "sops" ];
+      }
+      {
+        command = "sops --indent {{2}} --encrypt --in-place {{file}}";
+        alias = "sops encrypt";
+        description = "Encrypt file with sops";
+        tags = [ "sops" ];
+      }
+    ];
   };
 }
