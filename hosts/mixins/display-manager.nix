9b7fb5d9407a9a24a4cbc8ef08a01d91b1e7428e
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-03-04 09:08:15 +0100

    display-manager: switch from ly to greetd

--- hosts/mixins/display-manager.nix
+++ hosts/mixins/display-manager.nix
@@ -1,4 +1,9 @@
-{ lib, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   options.mixins.displayManager =
     let
@@ -8,14 +13,37 @@
       enable = mkEnableOption "displayManager config mixin";
     };
 
-  config.services =
+  config =
     let
       cfg = config.mixins.displayManager;
     in
     lib.mkIf cfg.enable {
-      displayManager = {
-        ly = {
-          enable = lib.mkDefault true;
+      programs.regreet = {
+        enable = true;
+
+        font = {
+          name = "Inter";
+          package = pkgs.inter;
+          size = 10;
+        };
+
+        theme = {
+          name = "Dracula";
+          package = pkgs.dracula-theme;
+        };
+
+        iconTheme = {
+          name = "Dracula";
+          package = pkgs.dracula-icon-theme;
+        };
+
+        cursorTheme = {
+          name = "Dracula-cursors";
+          package = pkgs.dracula-theme;
+        };
+
+        settings = {
+          application_prefer_dark_theme = true;
         };
       };
     };
