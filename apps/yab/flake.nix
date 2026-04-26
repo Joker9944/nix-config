4ae99bf49e615a03a01eb689032a8fc26d6fe9c6
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-04-26 13:49:47 +0200

    yab: fix waking from sleep issues again

--- apps/yab/flake.nix
+++ apps/yab/flake.nix
@@ -85,7 +85,7 @@
 
               mkdir -p $out/bin $out/share
               cp -r * $out/share
-              ags bundle app.ts $out/bin/${finalAttrs.name} -d "SRC='$out/share'"
+              ags bundle app.tsx $out/bin/${finalAttrs.name} -d "SRC='$out/share'"
 
               runHook postInstall
             '';
