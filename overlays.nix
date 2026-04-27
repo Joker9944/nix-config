5e36ee6c6451e024b28b19a8c1212ecfdb5cfe52
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-04-27 14:19:45 +0200

    openldap: init hack overlay

--- overlays.nix
+++ overlays.nix
@@ -7,4 +7,18 @@
   freelens = _: prev: {
     inherit (flake.packages.${prev.stdenv.hostPlatform.system}) freelens;
   };
+
+  openldap-fix = _: prev: {
+    openldap =
+      builtins.warn
+        ''
+          HACK(serene-franklin): openldap test runs are broken on nixpkgs
+          https://github.com/NixOS/nixpkgs/issues/513245
+        ''
+        (
+          prev.openldap.overrideAttrs (_: {
+            doCheck = !prev.stdenv.hostPlatform.isi686;
+          })
+        );
+  };
 }
