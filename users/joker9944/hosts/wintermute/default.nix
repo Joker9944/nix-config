b5efc1141cff633c39c40bdfdca08d791a72eb02
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-06-08 19:28:57 +0200

    wintermute: fix config

--- users/joker9944/hosts/wintermute/default.nix
+++ users/joker9944/hosts/wintermute/default.nix
@@ -1,17 +1,6 @@
 {
   programs._1password.gitSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFcft6944G+ygfWr5wT50TJUQ5f0dAKAr6H4QKSEAsUV";
 
-  mixins.desktopEnvironment.hyprland = {
-    system.environment = {
-      GDK_SCALE = 2;
-    };
-
-    waybar = {
-      battery = true;
-      stylus = true;
-    };
-  };
-
   programs.yas.config.battery = true;
 
   mixins.services.wayvnc.enable = true;
