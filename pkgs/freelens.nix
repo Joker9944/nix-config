318d9fc7d2c581827cf9355bdfea696e5d974daa
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-06-20 07:12:46 +0000

    freelens: 1.9.0 -> 1.10.0
    Diff: https://github.com/freelensapp/freelens/compare/v1.9.0...v1.10.0

--- pkgs/freelens.nix
+++ pkgs/freelens.nix
@@ -6,11 +6,11 @@
 }:
 let
   pname = "freelens";
-  version = "1.9.0";
+  version = "1.10.0";
 
   src = fetchurl {
     url = "https://github.com/freelensapp/${pname}/releases/download/v${version}/Freelens-${version}-linux-amd64.AppImage";
-    sha256 = "sha256-aM3sS87kpb9UhVMdG7cPeCp4j9KBW71H+MJR+oN4908=";
+    sha256 = "sha256-hrhRSU5X/eWZd7U3nt+AMJFAxZJtCzykWJALeWqpW2I=";
   };
 
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
