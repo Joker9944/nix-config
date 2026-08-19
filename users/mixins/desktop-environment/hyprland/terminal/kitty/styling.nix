51e43ec2cc9a9e3ed0e7d1e3f19adce86fd24b81
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-08-20 01:47:53 +0200

    feat(hyprland): use nix-schemes kitty module

--- users/mixins/desktop-environment/hyprland/terminal/kitty/styling.nix
+++ users/mixins/desktop-environment/hyprland/terminal/kitty/styling.nix
@@ -1,10 +1,8 @@
 { mkHyprlandModule, ... }:
-{ lib, config, ... }:
+{ config, ... }:
 let
   inherit (config.mixins.desktopEnvironment.hyprland.style) opacity;
   inherit (config.custom.theme) fonts;
-  inherit (config.schemes) scheme;
-  inherit (scheme) ansi;
 in
 mkHyprlandModule {
   programs.kitty = {
@@ -14,15 +12,8 @@ mkHyprlandModule {
       background_opacity = opacity.active;
     };
 
-    settings = {
-      cursor_shape = "beam";
-
-      cursor = "#${scheme.foreground.normal.hex}";
-      background = "#${scheme.background.normal.hex}";
-      foreground = "#${scheme.foreground.normal.hex}";
-    }
-    // (lib.concatMapAttrs (name: color: {
-      "color${toString (lib.fromHexString name)}" = "#${color.hex}";
-    }) ansi);
+    settings.cursor_shape = "beam";
   };
+
+  schemes.kitty.enable = true;
 }
