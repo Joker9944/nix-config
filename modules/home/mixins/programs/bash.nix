580c503de44c5af3a1d1d49dd1a316669aa6a7a0
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2026-09-04 23:48:08 +0200

    feat(mixins): customize eza aliases

--- modules/home/mixins/programs/bash.nix
+++ modules/home/mixins/programs/bash.nix
@@ -4,9 +4,6 @@ mkMixinModule "bash" {
     enable = true;
 
     shellAliases = {
-      ls = "ls --color=auto --human-readable";
-      ll = "ls --color=auto --human-readable -l --group-directories-first";
-      la = "ls --color=auto --human-readable -l --group-directories-first --all";
       grep = "grep --color=auto";
       ".." = "cd ..";
       "..." = "cd ../..";
--- modules/home/mixins/programs/eza.nix
+++ modules/home/mixins/programs/eza.nix
@@ -1,9 +1,18 @@
 { mkMixinModule, ... }:
-_:
+{ config, ... }:
 mkMixinModule "eza" {
   programs.eza = {
     enable = true;
-    git = true;
+    git = config.programs.git.enable;
     icons = "auto";
+  };
+
+  home.shellAliases = {
+    ls = "eza";
+    la = "eza --all";
+    ll = "eza --long --group-directories-first";
+    lla = "eza --all --long --group-directories-first";
+    lt = "eza --tree";
+    lta = "eza --all --tree";
   };
 }
